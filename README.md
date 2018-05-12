236647740048e32c6758ce2e6f9f7c11e1f13ede
timmyRS <tim@timmyrs.de>
2018-05-12 15:02:55 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# getargs.js
+
+A Javascript library which allows you to read and modify GET arguments using objects.
+
+[[Example]](https://hellshltd.github.io/getargs.js/) [[Download]](https://raw.githubusercontent.com/hellshltd/getargs.js/master/getargs.js)
---
+++ getargs.js
@@ -0,0 +1,43 @@
+/* getargs.js - Copyright (c) 2018, Hellsh Ltd. */
+
+if(!("getargs" in window.location))
+{
+	(function()
+	{
+		var getargs = undefined;
+		Object.defineProperty(window.location, "getargs",
+		{
+			get: function()
+			{
+				if(getargs === undefined)
+				{
+					getargs = {};
+					var searchArr = window.location.search.replace("?", "").split("&");
+					for(var i in searchArr)
+					{
+						var varArr = searchArr[i].split("=");
+						if(varArr.length == 2)
+						{
+							getargs[decodeURIComponent(varArr[0])] = decodeURIComponent(varArr[1]); 
+						}
+					}
+				}
+				return getargs;
+			},
+			set: function(newval)
+			{
+				getargs = newval;
+				var searchArr = [];
+				for(var key in getargs)
+				{
+					searchArr.push(encodeURIComponent(key) + "=" + encodeURIComponent(getargs[key]));
+				}
+				window.location.search = searchArr.join("&");
+			}
+		});
+	})();
+}
+else
+{
+	console.error("getargs.js failed to load: window.location.getargs is already defined.");
+}
