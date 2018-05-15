b9c2e28bb5219bdfd7e4ff6c6530583729d4d154
timmyRS <github@timmyrs.de>
2018-05-15 15:51:03 +0200

    Add options

--- getargs.js
+++ getargs.js
@@ -1,44 +1,57 @@
 /* getargs.js - Copyright (c) 2018, Hellsh Ltd. */
 
-if(!("getargs" in window.location))
+console.assert(!("getargsoptions" in window));
+console.assert("location" in window);
+console.assert(!("getargs" in window.location));
+
+window.getargsoptions = {
+	"reload": true /* If set to false, history.pushState will be used to avoid reload. */
+};
+
+(function()
 {
-	(function()
+	var getargs;
+	Object.defineProperty(window.location, "getargs",
 	{
-		var getargs = undefined;
-		Object.defineProperty(window.location, "getargs",
+		get: function()
 		{
-			get: function()
+			if(getargs === undefined)
 			{
-				if(getargs === undefined)
+				getargs = {};
+				var searchArr = window.location.search.replace("?", "").split("&");
+				for(var i in searchArr)
 				{
-					getargs = {};
-					var searchArr = window.location.search.replace("?", "").split("&");
-					for(var i in searchArr)
+					var varArr = searchArr[i].split("="), key = decodeURIComponent(varArr[0]);
+					if(key == "")
 					{
-						var varArr = searchArr[i].split("="), key = decodeURIComponent(varArr[0]);
-						if(key == "")
-						{
-							continue;
-						}
-						getargs[key] = (varArr.length == 2 ? decodeURIComponent(varArr[1]) : "");
+						continue;
 					}
+					getargs[key] = (varArr.length == 2 ? decodeURIComponent(varArr[1]) : "");
 				}
-				return getargs;
-			},
-			set: function(newval)
+			}
+			return getargs;
+		},
+		set: function(newval)
+		{
+			getargs = newval;
+			var searchArr = [];
+			for(var key in getargs)
 			{
-				getargs = newval;
-				var searchArr = [];
-				for(var key in getargs)
-				{
-					searchArr.push(encodeURIComponent(key) + "=" + encodeURIComponent(getargs[key]));
-				}
+				searchArr.push(encodeURIComponent(key) + "=" + encodeURIComponent(getargs[key]));
+			}
+			if(window.getargsoptions.reload)
+			{
 				window.location.search = searchArr.join("&");
 			}
-		});
-	})();
-}
-else
-{
-	console.error("getargs.js failed to load: window.location.getargs is already defined.");
-}
+			else
+			{
+				var url = window.location.pathname.toString() + "?" + searchArr.join("&"), hash = window.location.hash.toString().replace("#", "");
+				if(hash != "")
+				{
+					url += "#" + hash;
+				}
+				history.pushState({}, document.getElementsByTagName("title")[0].textContent, url);
+			}
+		}
+	});
+})();
