51dc369f758d87d88aa6913c7817183c7eadeee6
timmyRS <github@timmyrs.de>
2018-05-15 19:12:14 +0200

    Create 'window.getargs', documentation, & events.

--- README.md
+++ README.md
@@ -1,5 +1,38 @@
 # getargs.js
 
-A Javascript library which allows you to read and modify GET arguments using objects.
+A javascript library for easily reading and modifying GET arguments.
 
 [[Example]](https://hellshltd.github.io/getargs.js/) [[Download]](https://raw.githubusercontent.com/hellshltd/getargs.js/master/getargs.js)
+
+## Reading
+
+You can get the getargs using `window.getargs.get();` or `window.location.getargs;`.
+
+You can get a single property using `window.getargs.get(key);` or `window.location.getargs.key`. Either way, `undefined` means the property has not been set and an empty string means it's present but without value, e.g. `?test` or `?test=`.
+
+## Writing
+
+You can set the getargs to `undefined`, a string, or an object using `window.getargs.set(value);` or `window.location.getargs = value;`.
+
+You can set a single property using `window.getargs.set(key, value);`, which returns `window.getargs` allowing for code such as `window.getargs.set("key1", "value1").set("key2", "value2");`
+
+## Events
+
+You can register a change handler using `window.getargs.registerChangeHandler(function);` and unregister one using `window.getargs.registerChangeHandler(function);`.
+
+A change handler is a normal function which will be called every time the value of `location.search` changes.
+
+This is only useful if you have `window.getargs.options.pushState` enabled and fix links using `window.getargs.fixLinks()`.
+
+## Options
+
+You can modify the options by accessing the `window.getargs.options` object. There are the following properties:
+
+- `pushState` (boolean, default: false) If true, `history.pushState` will be used instead of `window.location.search` to prevent the site from reloading.
+- `monitorInterval` (int, default: 200) If you registered a change handler, a monitor "thread" will start and check `location.search` every `monitorInterval` milliseconds.
+
+## Utils
+
+- `window.getargs.searchToObject(search)` turns a string such as `?test=true` in an object such as `{"test":"true"}`.
+- `window.getargs.objectToSearch(object)` turns an object such as `{"test":"true"}` in a string such as `test=true`.
+- `window.getargs.fixLinks()` turns all links with a href such as `?test=true` into links which will call `window.getargs.set(href)`. This is only useful if you have `window.getargs.options.pushState` enabled.
--- getargs.js
+++ getargs.js
@@ -1,78 +1,155 @@
-/* getargs.js - Copyright (c) 2018, Hellsh Ltd. */
-
-console.assert(!("getargsoptions" in window));
-console.assert("location" in window);
-console.assert(!("getargs" in window.location));
-
-window.getargsoptions = {
-	"reload": true /* If set to false, history.pushState will be used to avoid reload. */
-};
+/* getargs.js v1.1 - Copyright (c) 2018, Hellsh Ltd. */
 
 (function()
 {
-	var searchToObject = function(search)
+	console.assert(typeof window != "undefined");
+	console.assert(!("getargs" in window));
+	console.assert("location" in window);
+	console.assert(!("getargs" in window));
+	var getargs_value, previous_search = window.location.search.toString(), changeHandlers = [],
+	changeMonitor = function()
 	{
-		var obj = {},
-		searchArr = search.replace("?", "").split("&");
-		for(var i in searchArr)
+		if(changeHandlers.length == 0)
 		{
-			var varArr = searchArr[i].split("="), key = decodeURIComponent(varArr[0]);
-			if(key == "")
-			{
-				continue;
-			}
-			obj[key] = (varArr.length == 2 ? decodeURIComponent(varArr[1]) : "");
+			return;
 		}
-		return obj;
-	},
-	getargs;
-	Object.defineProperty(window.location, "getargs",
-	{
-		get: function()
+		if(window.location.search.toString() != previous_search)
 		{
-			if(getargs === undefined)
+			previous_search = window.location.search.toString();
+			for(let i in changeHandlers)
 			{
-				getargs = searchToObject(window.location.search.toString());
+				changeHandlers[i]();
 			}
-			return getargs;
+		}
+		window.setTimeout(changeMonitor, window.getargs.options.monitorInterval);
+	};
+	window.getargs = {
+		options: {
+			"pushState": false,
+			"monitorInterval": 200
 		},
-		set: function(newval)
+		searchToObject: function(search)
 		{
-			if(newval === undefined)
+			let object = {},
+			searchArr = search.replace("?", "").split("&");
+			for(let i in searchArr)
 			{
-				getargs = {};
+				let varArr = searchArr[i].split("="), key = decodeURIComponent(varArr[0]);
+				if(key == "")
+				{
+					continue;
+				}
+				object[key] = (varArr.length == 2 ? decodeURIComponent(varArr[1]) : "");
 			}
-			else if(typeof newval == "object")
+			return object;
+		},
+		objectToSearch: function(object)
+		{
+			let searchArr = [];
+			for(let key in object)
 			{
-				getargs = newval;
+				searchArr.push(encodeURIComponent(key) + "=" + encodeURIComponent(object[key]));
 			}
-			else if(typeof newval == "string")
+			return searchArr.join("&");
+		},
+		fixLinks: function()
+		{
+			let links = document.querySelectorAll("a[href^='?']");
+			for(let i in links)
 			{
-				getargs = searchToObject(newval);
+				if(links[i] instanceof HTMLElement)
+				{
+					links[i].addEventListener("click", function(event)
+					{
+						event.preventDefault();
+						window.getargs.set(this.getAttribute("href"));
+					});
+				}
+			}
+			return window.getargs;
+		},
+		get: function(key)
+		{
+			if(getargs_value === undefined)
+			{
+				getargs_value = window.getargs.searchToObject(window.location.search.toString());
+			}
+			if(key !== undefined)
+			{
+				return getargs_value[key];
+			}
+			return getargs_value;
+		},
+		set: function(key, val)
+		{
+			if(val === undefined)
+			{
+				if(key === undefined)
+				{
+					getargs_value = {};
+				}
+				else if(typeof key == "object")
+				{
+					getargs_value = key;
+				}
+				else if(typeof key == "string")
+				{
+					getargs_value = window.getargs.searchToObject(key);
+				}
+				else
+				{
+					console.error("You can't set getargs to", typeof key);
+					return;
+				}
 			}
 			else
 			{
-				console.warn("Invalid getargs value:", newval);
-				return;
+				window.getargs.get()[key] = val;
 			}
-			var searchArr = [];
-			for(var key in getargs)
+			let search = window.getargs.objectToSearch(getargs_value);
+			if(window.getargs.options.pushState)
 			{
-				searchArr.push(encodeURIComponent(key) + "=" + encodeURIComponent(getargs[key]));
-			}
-			if(window.getargsoptions.reload)
-			{
-				window.location.search = searchArr.join("&");
-			}
-			else
-			{
-				var url = window.location.pathname.toString() + "?" + searchArr.join("&"), hash = window.location.hash.toString().replace("#", "");
+				var url = window.location.pathname.toString() + "?" + search, hash = window.location.hash.toString().replace("#", "");
 				if(hash != "")
 				{
 					url += "#" + hash;
 				}
 				history.pushState({}, document.getElementsByTagName("title")[0].textContent, url);
 			}
+			else
+			{
+				window.location.search = search;
+			}
+			return window.getargs;
+		},
+		registerChangeHandler: function(func)
+		{
+			changeHandlers.push(func);
+			if(changeHandlers.length == 1)
+			{
+				changeMonitor();
+			}
+			return window.getargs;
+		},
+		unregisterChangeHandler: function(func)
+		{
+			let i = changeHandlers.indexOf(func);
+			if(i != -1)
+			{
+				changeHandlers.splice(i, 1);
+			}
+			return window.getargs;
+		}
+	};
+	Object.defineProperty(window.location, "getargs",
+	{
+		get: function()
+		{
+			return window.getargs.get();
+		},
+		set: function(val)
+		{
+			window.getargs.set(val);
 		}
 	});
 })();
